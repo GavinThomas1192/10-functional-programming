a3bab7d157c0a297d06044c68f4a22f425449a87
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-24 10:32:55 -0700

    GavinDroveCameronNavigated

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -27,7 +27,7 @@ Article.all = [];
 Article.prototype.toHtml = function() {
   var template = Handlebars.compile($('#article-template').text());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
   this.body = marked(this.body);
 
@@ -35,13 +35,15 @@ Article.prototype.toHtml = function() {
 };
 
 Article.loadAll = rows => {
-  rows.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
+  rows.sort((a, b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)));
 
   // TODO: Refactor this forEach code, by using a `.map` call instead, since what we are trying to accomplish
   // is the transformation of one collection into another. Remember that we can set variables equal to the result
   // of functions. So if we set a variable equal to the result of a .map, it will be our transformed array.
   // There is no need to push to anything.
-
+  Article.all = rawData.map(ele => {
+    new Article(ele)
+  });
   /* OLD forEach():
   rawData.forEach(function(ele) {
   Article.all.push(new Article(ele));
@@ -52,27 +54,50 @@ Article.loadAll = rows => {
 
 Article.fetchAll = callback => {
   $.get('/articles')
-  .then(
-    results => {
-      Article.loadAll(results);
-      callback();
-    }
-  )
+    .then(
+      results => {
+        Article.loadAll(results);
+        callback();
+      }
+    )
 };
 
 // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
 Article.numWordsAll = () => {
-  return Article.all.map().reduce()
+  return Article.all.map(function(article) {
+    article.body.split(' ').length
+  }).reduce(function(sum, idx) {
+    return sum + idx;
+  })
 };
 
 // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names. You will
 // probably need to use the optional accumulator argument in your reduce call.
 Article.allAuthors = () => {
-  return Article.all.map().reduce();
+  return Article.all.map(article => {
+    return article.author
+  }).reduce((acc, current) => {
+    if (!acc.includes(current)) {
+      acc.push(current);
+    }
+    return acc;
+  }, [])
 };
+
+
 
 Article.numWordsByAuthor = () => {
   return Article.allAuthors().map(author => {
+    return {
+      name: author,
+      wordCount: Article.all.filter(function(article) {
+        return author === article.author
+      }).map(function(article) {
+        article.body.split(' ').length
+      }).reduce(function(sum, idx) {
+        return sum + idx;
+      })
+    }
     // TODO: Transform each author string into an object with properties for
     // the author's name, as well as the total number of words across all articles
     // written by the specified author.
@@ -90,16 +115,23 @@ Article.truncateTable = callback => {
     method: 'DELETE',
   })
   .then(console.log) // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
-                     // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
-                     // outside the scope of 301 material, but feel free to research!
+  // The reason we can do this has to do with the way Promise.prototype.then works. It's a little
+  // outside the scope of 301 material, but feel free to research!
   .then(callback);
 };
 
 Article.prototype.insertRecord = function(callback) {
   // REVIEW: Why can't we use an arrow function here for .insertRecord()??
-  $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
-  .then(console.log)
-  .then(callback);
+  $.post('/articles', {
+    author: this.author,
+    authorUrl: this.authorUrl,
+    body: this.body,
+    category: this.category,
+    publishedOn: this.publishedOn,
+    title: this.title
+  })
+    .then(console.log)
+    .then(callback);
 };
 
 Article.prototype.deleteRecord = function(callback) {
@@ -107,8 +139,8 @@ Article.prototype.deleteRecord = function(callback) {
     url: `/articles/${this.article_id}`,
     method: 'DELETE'
   })
-  .then(console.log)
-  .then(callback);
+    .then(console.log)
+    .then(callback);
 };
 
 Article.prototype.updateRecord = function(callback) {
@@ -125,6 +157,6 @@ Article.prototype.updateRecord = function(callback) {
       author_id: this.author_id
     }
   })
-  .then(console.log)
-  .then(callback);
+    .then(console.log)
+    .then(callback);
 };
